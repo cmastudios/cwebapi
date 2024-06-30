95fa7f2ae23c25950bcfa672729b4dbbf0a2968e
Connor Monahan <cma2714@gmail.com>
2024-06-29 18:54:38 -0700

    It works

--- api/main.cpp
+++ api/main.cpp
@@ -1,12 +1,72 @@
+#define CROW_STATIC_DIRECTORY "client/build/static/"
 #include "crow.h"
+#include "crow/middlewares/cors.h"
+#include <algorithm>
+#include <optional>
+
+struct User
+{
+    int id;
+    std::string username;
+};
+struct Database
+{
+    virtual std::vector<User> get_users() = 0;
+    virtual std::optional<User> get_user_by_id(unsigned id) = 0;
+};
+struct TestDatabase : public Database
+{
+    std::vector<User> get_users() override
+    {
+        return {User{1, "cmastudios"}};
+    }
+    std::optional<User> get_user_by_id(unsigned id) override
+    {
+        if (id == 1) return User{1, "cmastudios"};
+        return {};
+    }
+};
+crow::Blueprint user_blueprint(Database &db)
+{
+    crow::Blueprint bp("user");
+    auto serialize_user = [](const User &user) -> crow::json::wvalue {
+        return {{"id", user.id}, {"username", user.username}};
+    };
+    CROW_BP_ROUTE(bp, "/")([&]() -> crow::json::wvalue {
+        auto users = db.get_users();
+        std::vector<crow::json::wvalue> objects;
+        std::transform(users.cbegin(), users.cend(), std::back_inserter(objects), serialize_user);
+        return objects;
+    });
+    CROW_BP_ROUTE(bp, "/<uint>")([&](unsigned id) -> crow::response {
+        auto user = db.get_user_by_id(id);
+        if (user.has_value()) return crow::response(serialize_user(user.value()));
+        return crow::response(404, crow::json::wvalue::empty_object());
+    });
+    return bp;
+}
+
+#define RETURN_STATIC_FILE(path) [](){ auto resp = crow::response(); resp.set_static_file_info(path); return resp; }
 
 int main()
 {
-    crow::SimpleApp app;
+    crow::App<crow::CORSHandler> app;
+    TestDatabase db;
 
-    CROW_ROUTE(app, "/")([](){
-        return "Hello world";
+    CROW_ROUTE(app, "/")(RETURN_STATIC_FILE("client/build/index.html"));
+    CROW_ROUTE(app, "/favicon.ico")(RETURN_STATIC_FILE("client/build/favicon.ico"));
+    CROW_ROUTE(app, "/logo192.png")(RETURN_STATIC_FILE("client/build/logo192.png"));
+    CROW_ROUTE(app, "/logo512.png")(RETURN_STATIC_FILE("client/build/logo512.png"));
+    CROW_ROUTE(app, "/manifest.json")(RETURN_STATIC_FILE("client/build/manifest.json"));
+    CROW_ROUTE(app, "/asset-manifest.json")(RETURN_STATIC_FILE("client/build/asset-manifest.json"));
+    CROW_ROUTE(app, "/robots.txt")(RETURN_STATIC_FILE("client/build/robots.txt"));
+    crow::Blueprint apibp("api");
+    app.register_blueprint(apibp);
+    CROW_BP_ROUTE(apibp, "/")([](){
+        return 404;
     });
+    auto userbp = user_blueprint(db);
+    apibp.register_blueprint(userbp);
 
-    app.port(18080).multithreaded().run();
+    app.port(4000).multithreaded().run();
 }
--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:4000/api/user/")
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+        setUsers(data);
+    })
+    .catch((err) => {
+        console.log(err.message);
+    });
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +22,19 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h2>Users</h2>
+        <table>
+          <thead>
+            <tr><th>ID</th><th>Username</th></tr>
+          </thead>
+          <tbody>
+            {users.map((user) => {
+              return (
+                <tr key={user.id}><td>{user.id}</td><td>{user.username}</td></tr>
+              );
+            })}
+          </tbody>
+        </table>
         <a
           className="App-link"
           href="https://reactjs.org"
